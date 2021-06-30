03728452fbf6815a38bd1d10b31e7cdfb7e1af4c
Mike Evans <me.accenture@gmail.com>
2021-06-30 15:28:55 -0400

    4th commit

--- script.py
+++ script.py
@@ -4,15 +4,15 @@ import sys
 
 import requests
 
-#print (sys.version)
+print (sys.version)
 print(sys.executable)
 
 def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)
     return greeting
 
-#print(greet('World'))
-#print(greet('Mike'))
+print(greet('World'))
+print(greet('Mike'))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
